1d7b5d8cbaf61cdc56680276eeb66633fc4a0025
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-01-21 01:12:04 +0800

    update main.ts

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const input = document.getElementById("search-input") as HTMLInputElement;
+const button = document.getElementById("search-button") as HTMLButtonElement;
+
+const searchPokemon = () => {};
+
+button.addEventListener("click", searchPokemon);
